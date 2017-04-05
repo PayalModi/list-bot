b6b94b3341eac486758ec83c3024b654ff6071fa
PayalModi <payalmodi@Payals-MBP-2.wireless.yale.internal>
2017-04-05 17:32:46 -0400

    Add verification for webhook

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def homepage
+  	if params["hub.mode"] == 'subscribe' and params["hub.challenge"]
+  		render plain: params["hub.challenge"], status: 200
+  	else
+  		head 200
+  	end
   end
 end
