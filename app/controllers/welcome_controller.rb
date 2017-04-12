a400905775be88fc192b23c9861204aec4b8f937
PayalModi <payalmodi@Payals-MBP-2.wireless.yale.internal>
2017-04-12 14:31:11 -0400

    Put token in ev

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def homepage
-  	if params["hub.mode"] == 'subscribe' and params["hub.verify_token"] == 'token_fb_list_bot' and params["hub.challenge"]
+  	if params["hub.mode"] == 'subscribe' and params["hub.verify_token"] == ENV["VERIFY_TOKEN"] and params["hub.challenge"]
   		render plain: params["hub.challenge"], status: 200
   	else
   		head 200
