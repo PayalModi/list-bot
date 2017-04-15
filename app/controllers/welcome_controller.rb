993d363eff9acec595a8681f2616463b2b7a51d2
PayalModi <payalmodi@Payals-MBP-2.wireless.yale.internal>
2017-04-15 15:16:24 -0400

    Print POST params

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
   def homepage
-  	if params["hub.mode"] == 'subscribe' and params["hub.verify_token"] == ENV["VERIFY_TOKEN"] and params["hub.challenge"]
-  		render plain: params["hub.challenge"], status: 200
+  	if request.post?
+  		puts params
   	else
-  		head 200
+  		if params["hub.mode"] == 'subscribe' and params["hub.verify_token"] == ENV["VERIFY_TOKEN"] and params["hub.challenge"]
+	  		render plain: params["hub.challenge"], status: 200
+	  	else
+	  		head 200
+  		end
   	end
   end
 end
