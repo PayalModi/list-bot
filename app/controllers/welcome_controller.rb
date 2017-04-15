27194eeb790c8d6061a9c43bde8ac7ee15e0e490
PayalModi <payalmodi@Payals-MBP-2.wireless.yale.internal>
2017-04-15 16:48:31 -0400

    Extract text from JSON POST data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,10 @@
 class WelcomeController < ApplicationController
   def homepage
-  	puts "Reached homepage"
   	if request.post?
-  		puts params
   		information = request.raw_post
 		data_parsed = JSON.parse(information)
   		puts data_parsed
+  		puts data_parsed["entry"]["messaging"]["message"]["text"]
   	else
   		if params["hub.mode"] == 'subscribe' and params["hub.verify_token"] == ENV["VERIFY_TOKEN"] and params["hub.challenge"]
 	  		render plain: params["hub.challenge"], status: 200
