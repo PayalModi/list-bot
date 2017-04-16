aabd1209cf16394050e9bdae30aa3f3e1d69e5be
PayalModi <payalmodi@Payals-MBP-2.wireless.yale.internal>
2017-04-16 13:15:44 -0400

    Change post request

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,18 +13,8 @@ class WelcomeController < ApplicationController
 	  			for event in entry["messaging"] do
 	  				senderID = event["sender"]["id"]
 	  				messageText = event["message"]["text"]
-	  				puts messageText
-	  				puts senderID
 
-	  				uri = URI.parse("https://graph.facebook.com/v2.6/me/messages?access_token="+ENV["PAGE_ACCESS_TOKEN"])
-	  				header = {'Content-Type': 'application/json'}
-	  				response = {recipient: {id: senderID}, message: {text: messageText}}
-
-	  				http = Net::HTTP.new(uri.host, uri.port)
-	  				request = Net::HTTP::Post.new(uri.request_uri, header)
-	  				request.body = response.to_json
-	  				puts response
-	  				http.request(request)
+	  				Net::HTTP.post URI("https://graph.facebook.com/v2.6/me/messages?access_token="+ENV["PAGE_ACCESS_TOKEN"]), {:recipient => {:id => senderID}, :message => {:text => messageText}}.to_json, "Content-Type" => "application/json"
 	  			end
 	  		end
 	  	end
