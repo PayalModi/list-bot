2e6981da58e025d0531b86ce070afeb4dbb000ad
PayalModi <payalmodi@Payals-MBP-2.wireless.yale.internal>
2017-04-17 13:08:51 -0400

    Put into methods

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,24 +12,31 @@ class WelcomeController < ApplicationController
 	  			for event in entry["messaging"] do
 	  				senderID = event["sender"]["id"]
 	  				messageText = event["message"]["text"]
-
-	  				uri = URI.parse("https://graph.facebook.com/v2.6/me/messages?access_token="+ENV["PAGE_ACCESS_TOKEN"])
-	  				response = {:recipient => {:id => senderID}, :message => {:text => messageText}}
-
-	  				http = Net::HTTP.new(uri.host, uri.port)
-	  				http.use_ssl = true
-	  				request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
-	  				request.body = response.to_json
-	  				http.request(request)
+	  				send_message(senderId, messageText)
 	  			end
 	  		end
 	  	end
   	else
-  		if params["hub.mode"] == 'subscribe' and params["hub.verify_token"] == ENV["VERIFY_TOKEN"] and params["hub.challenge"]
-	  		render plain: params["hub.challenge"], status: 200
-	  	else
-	  		head 200
-  		end
+  		verify_webhook(params["hub.mode"], params["hub.verify_token"], params["hub.challenge"])
   	end
+  end
+
+  def send_message (senderID, messageText)
+  	uri = URI.parse("https://graph.facebook.com/v2.6/me/messages?access_token="+ENV["PAGE_ACCESS_TOKEN"])
+	response = {:recipient => {:id => senderID}, :message => {:text => messageText}}
+
+	http = Net::HTTP.new(uri.host, uri.port)
+	http.use_ssl = true
+	request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
+	request.body = response.to_json
+	http.request(request)
+  end
+
+  def verify_webhook (mode, token, challenge)
+  	if (mode == 'subscribe' and token == ENV["VERIFY_TOKEN"] and challenge)
+  		render plain: challenge, status: 200
+  	else
+  		head 200
+	end
   end
 end
