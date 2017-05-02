e156d48205524877841d34362c9447a0765c3d6c
PayalModi <payalmodi@Payals-MBP-2.wireless.yale.internal>
2017-05-02 14:28:57 -0400

    Show user list

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -31,7 +31,12 @@ class WelcomeController < ApplicationController
   				elsif messageText == "start new list"
   					responseText = "Ok! Starting a new list!"
   				elsif messageText == "show me the list"
-  					responseText = "This is the list so far:"
+  					responseText = "This is the list so far: "
+            items = ListItem.where(userid: senderID)
+            puts items
+            items.join(", ")
+            puts items
+            responseText = responseText + items
   				end
 
   				send_message(senderID, responseText)
