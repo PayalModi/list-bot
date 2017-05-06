f6470bc8751e1cc87a0bda5d3ffde8643275ed9c
PayalModi <payalmodi@Payals-MBP-2.wireless.yale.internal>
2017-05-06 19:54:37 -0400

    Change to quick reply options

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,61 +14,46 @@ class WelcomeController < ApplicationController
   end
 
   def respond_to_message (data_parsed)
-  	if data_parsed["object"] == 'page'
-  		for entry in data_parsed["entry"] do
-  			for event in entry["messaging"] do
+    if data_parsed["object"] == 'page'
+      for entry in data_parsed["entry"] do
+        for event in entry["messaging"] do
           senderID = event["sender"]["id"]
           if event["message"]
-    				messageText = event["message"]["text"]
-    				responseText = "Sorry I don't understand that yet. Try one of these phrases: start new list, show me the list, or add *"
-    				messageText = messageText.downcase
-
+            messageText = event["message"]["text"]
+            messageText = messageText.downcase
             if event["message"]["quick_reply"]
-              puts "Quick reply option clicked"
-              responseText = "Ok. Deleting that item"
-              send_text_message(senderID, responseText)
+              if messageText == "start new list"
+                items = ListItem.where(userid: senderID)
+                for item in items do
+                  item.destroy
+                end
+                send_text_message(senderID, "Ok! Starting a new list!")
+              elsif messageText == "show me the list"
+                responseText = "This is the list so far:"
+                items = ListItem.where(userid: senderID)
+                for item in items do
+                  responseText = responseText + "\n" + item[:itemname]
+                end
+                send_text_message(senderID, responseText)
+              elsif messageText == "delete an item"
+                send_delete_quick_reply(senderID)
+              else
+                send_text_message(senderID, "Ok. Deleting that item.")
+              end
 
-    				elsif messageText.start_with?("add")
-    					item = messageText[4..-1]
+            elsif messageText.start_with?("add")
+              item = messageText[4..-1]
               ListItem.create(:itemname => item, :created_at => DateTime.now, :userid => senderID)
-    					responseText = "Ok! I added " + item + " to the list."
+              responseText = "Ok! I added " + item + " to the list."
               send_text_message(senderID, responseText)
-
-    				elsif messageText == "hi" or messageText == "hello" or messageText == "hey"
-    					responseText = "Hi there!"
-              send_text_message(senderID, responseText)
-
-    				elsif messageText == "start new list"
-    					responseText = "Ok! Starting a new list!"
-              items = ListItem.where(userid: senderID)
-              for item in items do
-                item.destroy
-              end
-              send_text_message(senderID, responseText)
-
-    				elsif messageText == "show me the list"
-    					responseText = "This is the list so far:"
-              items = ListItem.where(userid: senderID)
-              for item in items do
-                responseText = responseText + "\n" + item[:itemname]
-              end
-              send_text_message(senderID, responseText)
-
-            elsif messageText == "delete an item"
-              send_quick_reply(senderID)
 
             else
-              send_text_message(senderID, responseText)
-    				end
-
-          elsif event["postback"]
-            payload = event["postback"]["payload"]
-            puts "Got postback " + payload
-            send_text_message(senderID, "Deleting soda")
+              send_initial_quick_reply(senderID)
+            end
           end
-  			end
-  		end
-  	end
+        end
+      end
+    end
   end
 
   def send_text_message (senderID, messageText)
@@ -81,7 +66,13 @@ class WelcomeController < ApplicationController
     send_message (response)
   end
 
-  def send_quick_reply (senderID)
+  def send_initial_quick_reply(senderID)
+    response = {:recipient => {:id => senderID}, :message => {:text => "Pick one of these options, or type add * to add something to the list.", :quick_replies => [{:content_type => "text", :title => "Start new list", :payload => "new list"}, {:content_type => "text", :title => "Show me the list", :payload => "show list"}, {:content_type => "text", :title => "Delete an item", :payload => "delete item"}
+]}}
+    send_message(response)
+  end
+
+  def send_delete_quick_reply (senderID)
     response = {:recipient => {:id => senderID}, :message => {:text => "Pick an item to delete", :quick_replies => [{:content_type => "text", :title => "Soda", :payload => "Chose soda"}, {:content_type => "text", :title => "Potatoes", :payload => "Chose potatoes"}]}}
     send_message(response)
   end
